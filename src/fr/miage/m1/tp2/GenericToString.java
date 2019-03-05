c585cee08463739a304cc53f9863b6a7e4652fe0
bah <salioubah77@gmail.com>
2019-03-05 15:51:52 +0100

    Création de la methode toString

---
+++ src/fr/miage/m1/tp2/GenericToString.java
@@ -0,0 +1,93 @@
+/**
+ * 
+ */
+package fr.miage.m1.tp2;
+
+import java.awt.Point;
+import java.awt.Polygon;
+import java.lang.reflect.Field;
+
+/**
+ * @author BAH
+ *
+ */
+public class GenericToString {
+
+	/**
+	 * 
+	 */
+	public GenericToString() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	/*
+	 * Methode toString sans profondeur
+	 */
+	public String toString(Object ob) {
+
+		Class cl = ob.getClass();
+		Field attributs[] = cl.getDeclaredFields();
+
+		String affichage = cl.getName()+"[";
+		for (Field field : attributs) {
+			field.setAccessible(true);
+			try {
+				affichage += field.getName()+"="+field.get(ob)+";";
+			} catch (SecurityException e) {
+				e.printStackTrace();
+			} catch (IllegalArgumentException e) {
+
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+
+				e.printStackTrace();
+			}
+		}
+		affichage += "]";
+
+		return affichage;
+	}
+
+	/*
+	 * Methode toString avec profondeur
+	 */
+	public String toString(Object ob, int prof) throws IllegalArgumentException, IllegalAccessException, InstantiationException {
+
+		String affichage = "";
+		if(prof == 0) {
+			return affichage;
+		}
+		Class cl = ob.getClass();
+		Field attributs[] = cl.getDeclaredFields();
+
+		affichage = cl.getName()+"[";
+		for (Field field : attributs) {
+			field.setAccessible(true);
+			if(field.getType().isPrimitive()) {
+				affichage += field.getName()+"="+field.get(ob)+";";
+			}
+			else {
+				//	for (int i = 1; i < prof; i++) {
+				affichage += toString(field.get(ob),prof-1);
+				//}
+			}
+		}
+		affichage += "]";
+
+		return affichage;
+	}
+
+	public static void main(String[] args) {
+		GenericToString g = new GenericToString();
+		Polygon pol = new Polygon(new int[]{10, 20, 30}, new int[]{20,30, 40}, 3);  
+		pol.getBounds();
+		try {
+			System.out.println(g.toString(pol,2));
+		} catch (IllegalArgumentException | IllegalAccessException | InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
